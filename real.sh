4d8d4fb2e3a55c99d1b72b03120def6f533d2d3e
fredouille1910 <thibault.joubert.lafont@gmail.com>
2025-12-16 15:04:40 +0100

    Add files via upload

--- real.sh
+++ real.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 awk -F ';' '$1=="-" && $2!="-" && $3!="-" && $4!="-" && $5!="-" {print $2 ";" $3 ";" $4 ";" $5}' $1 > Real.txt
-#appeler le c et avoir un ficier avec que 2 valeur #changer le "Real.txt" l.12
+
+./wildwater script.sh $1 histo real Real.txt
+
 gnuplot << EOF
 set datafile separator ";"
 set terminal pngcairo size 800,600
@@ -10,7 +12,7 @@ set title "historeal"
 set ylabel "k.m^3.year^{-1}"
 set style data histograms
 set style fill solid 1.0 border -1
-plot "Real.txt" using 2:xtic(1) title "Donnes"
+plot "returnreal.txt" using 2:xtic(1) title "Donnes"
 EOF
 
 echo "Histogramme généré : histogram_real.png"
--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@ if [ -f $1 ] && [ "$2" = "histo" ] && [ "$3" = "max" ];then
 bash max.sh $1  
 elif [ -f $1 ] && [ "$2" = "histo" ] && [ "$3" = "src" ]; then
 bash src.sh $1
-# elif [ -f $1 ] && [ "$2" = "histo" ] && [ "$3" = "real" ]; then
-#bash real.sh $1
+elif [ -f $1 ] && [ "$2" = "histo" ] && [ "$3" = "real" ]; then
+bash real.sh $1
 #elif pour leaks
 
 else
