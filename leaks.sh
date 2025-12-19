aaba879037e9cb23529c1c1735c448c929fb2d8a
fredouille1910 <thibault.joubert.lafont@gmail.com>
2025-12-19 13:31:56 +0100

    Add files via upload

--- leaks.sh
+++ leaks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+awk -F ';' '$1!="-" && $2!="-" && $3!="-" && $4=="-" && $5!="-" {print $2 ";" $3 ";" $4 ";" $5}' $1 > Facility.txt
+
 NomUsine=$2
 resultat=$(awk -F ';' -v usine="$NomUsine" '$5=="-" && $2==usine { print "Usine existe" }' "$1") 
 if [ "$resultat" = "Usine existe" ];then
