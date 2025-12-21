86fc12c6eb9ce5805a4c5bf1215dff7bfc25ed1c
fredouille1910 <thibault.joubert.lafont@gmail.com>
2025-12-21 18:36:28 +0100

    Update real.sh

--- real.sh
+++ real.sh
@@ -3,17 +3,31 @@
 awk -F ';' '$1=="-" && $2!="-" && $3!="-" && $4!="-" && $5!="-" {print $2 ";" $3 ";" $4 ";" $5}' $1 > SourceL.txt
 awk -F ';' '$1=="-" && $2!="-" && $3=="-" && $4!="-" && $5=="-" {print $2 ";" $4}' $1 > Usine.txt
 
-./wildwater script.sh $1 histo real Real.txt
+./wildwater "$1" histo real SourceL.txt
 
 gnuplot << EOF
 set datafile separator ";"
-set terminal pngcairo size 800,600
-set output "histogram_real.png"
+set terminal pngcairo size 1920,1080
+set output "histogram_real50.png"
 set title "historeal"
 set ylabel "k.m^3.year^{-1}"
 set style data histograms
 set style fill solid 1.0 border -1
-plot "returnreal.txt" using 2:xtic(1) title "Donnes"
+set xtics rotate by 60 right
+plot "< sort -t';' -k2,2n returnreal.txt 2>/dev/null | head -n 50" using 2:xtic(1) title "Donnees"
 EOF
 
-echo "Histogramme généré : histogram_real.png"
+
+gnuplot << EOF
+set datafile separator ";"
+set terminal pngcairo size 1920,1080
+set output "histogram_real10.png"
+set title "historeal"
+set ylabel "k.m^3.year^{-1}"
+set style data histograms
+set style fill solid 1.0 border -1
+set xtics rotate by 60 right
+plot "< sort -t';' -k2,2n returnreal.txt 2>/dev/null | tail -n 10" using 2:xtic(1) title "Donnees"
+EOF
+
+echo "Histogrammes générés : histogram_real10.png et histogram_real50.png"
