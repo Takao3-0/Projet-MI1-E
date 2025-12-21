a0fd54df6ed4e4b8af78bb915e6aed1dbd34b7e2
fredouille1910 <thibault.joubert.lafont@gmail.com>
2025-12-21 18:00:21 +0100

    Update leaks.sh

--- leaks.sh
+++ leaks.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-awk -F';' -v OFS=';' '$4=="-" && $1!="-" && $2!="-" && $3!="-" && $5!="-" {print $2, $3, "-", $5}' "$1" > Facility.txt
+awk -F';' -v OFS=';' -v usine="$NomUsine" '($1==usine || $2==usine) && $4=="-" && $5!="" {print $1, $2, $3, "-", $5 > "Facility.txt"} $1=="-" && $2!="" && $3!="" && $4!="" && $5!="" {print $2, $3, $4, $5 > "SourceL.txt"}' "$1"
+
 NomUsine=$2
-if awk -F';' -v usine="$NomUsine" '$5=="-" && $2==usine { exit 0 } END { exit 1 }' "$1"; then
-echo "Usine existante"
-#./wildwater "$1" rajouter les fichier pour alex
-else 
-	echo "Nom d'usine incorrect"
+if if grep -q "^.*;$NomUsine;.*;.*;-$" "$1"; then
+    echo "Usine existante"
+	#./wildwater "$1" rajouter les fichier pour alex
+else
+    echo "Nom d'usine incorrect"
 fi
-
-
-
-
