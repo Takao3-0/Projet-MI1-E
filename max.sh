669b6b4e73210599d45e624f7bccbe33fef7621a
fredouille1910 <thibault.joubert.lafont@gmail.com>
2025-12-21 18:46:53 +0100

    Update max.sh

--- max.sh
+++ max.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-awk -F ';' '$1=="-" && $2!="-" && $3=="-" && $4!="-" && $5=="-" {print $2 ";" $4}' $1 > Usine.txt
+awk -F ';' -v OFS=';' '$1=="-" && $2!="-" && $3=="-" && $4!="-" && $5=="-" {print $2, $4}' $1 > Usine.txt
 
 gnuplot << EOF
 set datafile separator ";"
